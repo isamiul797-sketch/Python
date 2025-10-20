52d6ca31ff0c95df5d8fd7b575a0a627ab1c5f25
Samiul Islam Sami <isamiul797@gmail.com>
2025-10-20 10:57:58 +0600

    added new topic

---
+++ PYTHON/userinput.py
@@ -0,0 +1,9 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+gpa = input("Enter your gpa:")
+
+print("student Information")
+print("____________________")
+print("Name : "+name)
+print("age : "+age)
+print("GPA : "+gpa)
