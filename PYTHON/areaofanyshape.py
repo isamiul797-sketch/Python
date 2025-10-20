c04b344c86bb9b475634fdceabf87bb37835d12a
Samiul Islam Sami <isamiul797@gmail.com>
2025-10-20 11:38:31 +0600

    added new topic

---
+++ PYTHON/areaofanyshape.py
@@ -0,0 +1,25 @@
+#Area of triangle
+
+base = float(input("Enter base : "))
+height = float(input("Enter height : "))
+
+area1 = 0.5 * base * height
+print(area1)
+
+#Area od reactangle
+width = float(input("Enter width : "))
+height = float(input("Enter height: "))
+
+area2 = width * height
+print(area2)
+
+#Area of square
+a = int(input("Enter A : "))
+area3 = a*a
+print(area3)
+
+#Area of Circle
+radius = float(input("Enter Radius"))
+
+area4 = 3.1416 * radius*radius
+print(area4)
