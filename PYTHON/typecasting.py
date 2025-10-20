8c9625d868b62dae819fcbd2ca2f44601c593bee
Samiul Islam Sami <isamiul797@gmail.com>
2025-10-20 11:08:12 +0600

    added another new topic

---
+++ PYTHON/typecasting.py
@@ -0,0 +1,20 @@
+num1 = int(input("Enter your first num:"))
+num2 = int(input("Enter your secound num:"))
+
+result = num1 + num2
+print("The result is :" ,result)
+
+result = num1 - num2
+print("The result is :" ,result)
+
+result = num1 * num2
+print("The result is :" ,result)
+
+result = num1 / num2
+print("The result is :" ,result)
+
+result = num1 // num2
+print("The result is :" ,result)
+
+result = num1 ** num2
+print("The result is :" ,result)
