75579bee18a9c38669b20e38124b15ae9222f3de
Samiul Islam Sami <isamiul797@gmail.com>
2025-10-21 12:51:58 +0600

    added new file

---
+++ PYTHON/if&elsestatement.py
@@ -0,0 +1,24 @@
+marks = 56
+
+if marks >= 33:
+    print("Pass")
+
+else:
+    print("Fail")
+
+num1 = 90
+num2 = 450
+
+if num1 < num2:
+    print(num2)
+
+else:
+    print(num1)
+
+#even/odd
+num = 9
+
+if num%2 == 0:
+    print("Even")
+else:
+    print("Odd")
