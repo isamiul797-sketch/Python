3eeb5efcd9d8dbf7fb59cb7dc20f62f8fd45fa1d
Samiul Islam Sami <isamiul797@gmail.com>
2025-10-21 12:23:30 +0600

    added new topic

---
+++ PYTHON/demo.py
@@ -0,0 +1,15 @@
+num1 = 5
+num2 = 7.5
+name = "samiul islam sami"
+
+print(type(num1))
+print(type(num2))
+print(type(name))
+print(type(True))
+
+
+print(num1 + num2)
+print(f"{num1} + {num2} = {num1+num2}")
+
+print("samiul islam sami", end=" ")
+print("01778861341")
