b69855abe08a59e74b028f48fb00988bee9fbc6c
Samiul Islam Sami <isamiul797@gmail.com>
2025-10-22 14:45:10 +0600

    new topic

---
+++ PYTHON/logicaloperator.py
@@ -0,0 +1,22 @@
+num1 = 900
+num2 = 850
+num3 = 700
+
+if num1 > num2 and num1 > num3 :
+    print(num1)
+
+elif num2 > num1 and num2 > num3 :
+    print(num2)
+
+else:
+    print(num3)
+
+#vowel a,e,i,o,u
+
+ch = 'a'
+
+if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+    print("VOWEL")
+
+else :
+    print("CONSONENT")
