20a54f7b3b560102f74cd61de572e49752d03989
Samiul Islam Sami <isamiul797@gmail.com>
2025-11-08 18:26:38 +0600

    new topics

---
+++ PYTHON/list.py
@@ -0,0 +1,10 @@
+subjects = ["C","C++","Python","Java","Swift","Ruby","Javascript","C#"]
+
+print(subjects)
+print(subjects[0])
+print(subjects * 3)
+print(subjects[3])
+print(subjects[3:])
+print(subjects[-2])
+print(subjects[-4:])
+print(subjects+["IOS","Android",27])
