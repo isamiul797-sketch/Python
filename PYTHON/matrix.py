12cd72314b477a0b294803dbd8a34f44ff92705d
Samiul Islam Sami <isamiul797@gmail.com>
2025-11-11 19:13:22 +0600

    matrix

---
+++ PYTHON/matrix.py
@@ -0,0 +1,15 @@
+matrix = [
+    [1,2,3],
+    [4,5,6]
+]
+matrix[1][1] = 15
+print(matrix[1][1])
+
+matrix = [
+    [1,2,3],
+    [4,5,6]
+]
+
+for col in matrix:
+    for row in col:
+        print(row)
