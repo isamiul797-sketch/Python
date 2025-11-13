83a85b045fa975cbb118bb55f0a8f940a8933225
Samiul Islam Sami <isamiul797@gmail.com>
2025-11-13 18:13:45 +0600

    stack

---
+++ PYTHON/stack&queue.py
@@ -0,0 +1,16 @@
+books = []
+books.append("learn C")
+books.append("learn C++")
+books.append("learn Java")
+
+print(books)
+
+books.pop()
+print(books)
+
+books.pop()
+print(books)
+
+books.pop()
+if not books :
+    print("No Book Is Left")
