d9e09166c1434d8934c1f126d497cbf94050d96f
Samiul Islam Sami <isamiul797@gmail.com>
2025-11-13 18:24:33 +0600

    queue

--- PYTHON/stack&queue.py
+++ PYTHON/stack&queue.py
@@ -1,3 +1,5 @@
+#Stack - last in first out
+
 books = []
 books.append("learn C")
 books.append("learn C++")
@@ -14,3 +16,20 @@ print(books)
 books.pop()
 if not books :
     print("No Book Is Left")
+
+#Queue - first in first out
+
+from collections import deque
+
+bank = deque(["Anik","Sanu","Mamu"])
+print(bank)
+
+bank.popleft()
+print(bank)
+
+bank.popleft()
+print(bank)
+
+bank.popleft()
+if not bank:
+    print("No Person Is Left")
