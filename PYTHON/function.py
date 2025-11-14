06aaa043ba2dbc2413b90db0ae97b36b0f18af10
Samiul Islam Sami <isamiul797@gmail.com>
2025-11-14 18:06:00 +0600

    function

---
+++ PYTHON/function.py
@@ -0,0 +1,19 @@
+def add (x,y):
+    sum = x * y
+    print(sum)
+
+def addition(x,y,z):
+    sum = x + y + z
+    print(sum)
+
+def sub (a,b):
+    sub = a - b
+    print(sub)
+
+def message():
+    print("No Message is Available")
+
+add(20,80)
+sub(190,40)
+addition(21,70,90)
+message()
