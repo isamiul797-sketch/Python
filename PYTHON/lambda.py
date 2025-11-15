5056faee4c7ba97b876875977ba87749bfb5f7f2
Samiul Islam Sami <isamiul797@gmail.com>
2025-11-15 18:29:15 +0600

    lambda finished

--- PYTHON/lambda.py
+++ PYTHON/lambda.py
@@ -1,4 +1,4 @@
-def calculate(a,b):
-    return a*a + 2*a*b + b*b
-
-print(calculate(4,3))
+value1 = (lambda a,b:a*a + 2*a*b + b*b)(4,3)
+value2 = (lambda x: x*x*x )(5)
+print(value1)
+print(value2)
