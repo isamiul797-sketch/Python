7de65cc0a4c1e031c32ca6313fd958802eeb0934
Samiul Islam Sami <isamiul797@gmail.com>
2025-11-18 18:33:00 +0600

    write in a file

---
+++ PYTHON/write_file.py
@@ -0,0 +1,11 @@
+file = open("PYTHON/student.txt","a")
+
+file.write("\nSaniullah - 244 - 2.98")
+
+file.close()
+
+file = open("PYTHON/html.txt","a") 
+
+file.write("\nSaniullah - 244 - 2.98")
+
+file.close()
