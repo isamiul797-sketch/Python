6db8a6d7b9f4d359c76a46421d5f1a303c7d7101
Samiul Islam Sami <isamiul797@gmail.com>
2025-11-20 18:09:03 +0600

    class and object

---
+++ PYTHON/classobj.py
@@ -0,0 +1,13 @@
+class Student:
+    roll = ""
+    cgpa = ""
+
+sami = Student()
+sami.roll = 101
+sami.cgpa = 3.89
+print(f"Roll : {sami.roll}, CGPA : {sami.cgpa}")
+
+sani = Student()
+sani.roll = 401
+sani.cgpa = 3.23
+print(f"Roll : {sani.roll}, CGPA : {sani.cgpa}")
