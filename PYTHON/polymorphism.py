3ad47344d4db24d43ae452b733fade9cbe59f8f1
Samiul Islam Sami <isamiul797@gmail.com>
2025-11-23 18:06:13 +0600

    polymorphism function

---
+++ PYTHON/polymorphism.py
@@ -0,0 +1,12 @@
+#built in polymorphism function
+
+print(len("Samiul Islam Sami"))
+print(len([20,30,40]))
+
+#user defined polymorphism function
+
+def add(x, y, z=0):
+    return x + y + y
+
+print(add(10,20))
+print(add(10,20,30))
