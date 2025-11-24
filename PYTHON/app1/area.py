8a6cac07b052a8e351bd382b1ecdc023ad1f5c22
Samiul Islam Sami <isamiul797@gmail.com>
2025-11-24 18:23:38 +0600

    make a demo module

---
+++ PYTHON/app1/area.py
@@ -0,0 +1,5 @@
+def triangle_area(b,h):
+    print(f"Area of triangle is : {0.5 * b * h}")
+
+def ractangle_area(a,b):
+    print(f"Area of ractangle is : {a * b}")
---
+++ PYTHON/app1/test.py
@@ -0,0 +1,4 @@
+from area import *
+
+triangle_area(10,90)
+ractangle_area(30,40)
